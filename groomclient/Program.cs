5f38f9b50553e1a5e3494f7e04d0b87ae2338af6
Shervin <sh.montajam@hotmail.com>
2022-10-23 22:25:06 +0200

    Init

---
+++ groomclient/Program.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Net.Client;
+using gRoom.gRPC.Messages;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5210/");
+var client = new Groom.GroomClient(channel);
+
+Console.Write("Enter your room name");
+var roomName = Console.ReadLine();
+
+var response = client.RegisterToRoom(new RoomRegistrationRequest { RoomName = roomName });
+Console.WriteLine($"Room id is: {response}");
+
+Console.ReadLine();
+
